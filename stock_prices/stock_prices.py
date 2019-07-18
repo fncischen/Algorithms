d249482a269f34ca5fa2d59c84240742539612ac
Francis Chen <fncis.chen@gmail.com>
2019-07-18 14:22:15 -0700

    constrained the buy and sell price by making sure the price difference is based on the price item AFTER, not before, the price

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,26 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  amountOfStockPrices = len(prices)
+
+  stockDifferences = [[] for i in range(amountOfStockPrices)]
+
+  i = 0 
+  for price in prices: 
+    for i in range(prices.index(price), len(prices)):
+      priceDifference = prices[i] - price
+      stockDifferences[i].append(priceDifference)
+    i += 1
+
+  maxPrices = []
+
+  for priceDifferences in stockDifferences:
+    maxPrices.append(max(priceDifferences))
+  
+  maxProfit = max(maxPrices)
+  return maxProfit
+
+print(find_max_profit([1050, 270, 1540, 3800, 2]))
 
 # assume low price at buy, sell high. 
 
@@ -19,6 +38,8 @@ def find_max_profit(prices):
     # [ [ differnece btwn a2 and a1, a3...]]
     
     # use sort 
+
+  # you must buy first then sell. you cant buy anything after the first price you sell, but you can sell what's afterwards. 
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
